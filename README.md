be1279671f5caca3a666b9bee27560663b7b40da
Christopher Oezbek <c.oezbek@gmail.com>
2020-11-23 09:16:47 +0100

    [FEATURE] Allows systemd --user services to be used (new default)
    [FIX] Fixed a bug in the template that made unicorn run as root in system mode.
    
    [RELEASE] Bump to 0.9

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ This gem assumes that you
 * Are on a Linux system with systemd in /etc/systemd/
 * Are using RVM to manage your Ruby version
 * That the setup task has sudoer or root permissions or that you are running with execution_mode :system and are entering passwords. If your regular user does not have sudo, then you can set the `setup_user` to the name of a user who does.
-* Want to run nginx in front and that you want to update nginx configuration to point to unicorn instead.
 
 ## Basic installation instructions
 
@@ -32,13 +31,17 @@ require 'mina/nginx'
 require 'mina/unicorn'
 ```
 
-3. Manually run `'mina unicorn:setup'` or invoke from your own setup task.
+3. Manually run `'mina unicorn:setup'` or invoke from your own setup task to enable all one time tasks.
 
-4. Manually run `'mina unicorn:restart'` or invoke from your own deploy task.
+4. Once you are ready to enable unicorn run `'mina unicorn:enable'` to enable the systemd service (persists across reboots).
+
+5. To manually restart unicorn after a deploy use `'mina unicorn:restart'`.
 
 ## Customization
 
 To change the unicorn systemd service template and/or the `unicorn_conf.rb`, run `'mina unicorn:generate'`.
+
+By default `mina-unicorn_systemd` is running unicorn as a user service (`systemd --user`), but the unicorn service template also includes settings for running as a system service. To enable this, add `set :unicorn_system_or_user, 'system'` to the top of your `deploy.rb`.
 
 ## Contributing
 
--- lib/mina/unicorn.rb
+++ lib/mina/unicorn.rb
@@ -6,7 +6,16 @@ require "mina/unicorn/version"
 namespace :unicorn do
   
   set :unicorn_service_name,        -> { "unicorn-#{fetch(:application_name)}.service" }
-  set :unicorn_systemd_config_path, -> { "/etc/systemd/system/#{fetch :unicorn_service_name}" }
+  
+  set :unicorn_system_or_user,      -> { "user" }
+
+  set :unicorn_systemd_config_path, -> {    
+    if is_unicorn_user_installation?
+      "$HOME/.config/systemd/user/#{fetch :unicorn_service_name}" 
+    else
+      "/etc/systemd/system/#{fetch :unicorn_service_name}"
+    end
+  }
   
   set :nginx_socket_path,           -> { "#{fetch(:shared_path)}/unicorn.sock" }
   
@@ -27,48 +36,70 @@ namespace :unicorn do
     end
   end
   
-  desc "Setup Unicorn systemd service on the remote server"
+  desc "Setup Unicorn systemd service on the remote server (but doesn't start it)"
   task :setup do
   
-    elevate
-  
-    comment %(Check for systemd on remote server)    
-    command %([[ `systemctl` =~ -\.mount ]] || { echo 'Systemd not found, but mina-unicorn_systemd needs it.'; exit 1; }) # From https://unix.stackexchange.com/a/164092
-    
+    # Fetch these before elevation!
+    username = fetch(:user)
     target_path = fetch :unicorn_systemd_config_path
+    template = erb template_path
 
-    comment %(Installing unicorn systemd config file to #{target_path})
-    command %(echo '#{erb template_path}' > #{target_path})
+    unicorn_elevate do 
+      run :remote do 
 
-    comment %(Reloading systemd configuration)
-    command %(systemctl daemon-reload)
+        comment %(Check for systemd on remote server)
+        command %([[ `systemctl` =~ -\.mount ]] || { echo 'Systemd not found, but mina-unicorn_systemd needs it.'; exit 1; }) # From https://unix.stackexchange.com/a/164092
+
+        if is_unicorn_user_installation?
+          comment %(Check for libpam on remote server)
+          command %(DEBIAN_FRONTEND=noninteractive apt -yqq install libpam-systemd)
+      
+          comment %(Enable linger for systemd --user)    
+          command %(loginctl enable-linger #{username} || { echo 'Could not enable linger for user #{username} but mina-unicorn_systemd needs it.'; exit 1; }) 
+        end  
+
+        comment %(Installing unicorn systemd config file to #{target_path})
+        command %(mkdir -p $(dirname #{target_path}))
+        command %(echo '#{template}' > #{target_path})
+
+        comment %(Reloading systemd configuration)
+        command %(systemctl#{" --user" if is_unicorn_user_installation?} daemon-reload || { echo 'If this command fails in user mode then you likely have disabled UsePAM in your /etc/ssh/sshd_config which is needed'; exit 1; }) # https://superuser.com/a/1561345/32638
+
+      end
+    end
     
   end
   
   desc "Get the status of the Unicorn systemd service on the remote server"
   task :status do
-    command %(systemctl status #{fetch :unicorn_service_name})
+    command command_systemd_raw("status")
   end
   
-  desc "Start the Unicorn systemd service on the remote server"
-  task :start do
-    elevate
-    # Start the service and if it fails print the error information
-    command %((systemctl start #{fetch :unicorn_service_name} && systemctl status #{fetch :unicorn_service_name}) || journalctl --no-pager _SYSTEMD_INVOCATION_ID=`systemctl show -p InvocationID --value #{fetch :unicorn_service_name}`)
-  end
-  
-  desc "Stop the Unicorn systemd service on the remote server"
-  task :stop do
-    elevate
-    command %((systemctl stop #{fetch :unicorn_service_name} && systemctl status #{fetch :unicorn_service_name}) || journalctl --no-pager _SYSTEMD_INVOCATION_ID=`systemctl show -p InvocationID --value #{fetch :unicorn_service_name}`)
-  end
-  
-  desc "Restart the Unicorn systemd service on the remote server"
-  task :restart do
-    elevate
-    command %((systemctl restart #{fetch :unicorn_service_name} && systemctl status #{fetch :unicorn_service_name}) || journalctl --no-pager _SYSTEMD_INVOCATION_ID=`systemctl show -p InvocationID --value #{fetch :unicorn_service_name}`)
-  end 
-  
+  %w(start stop restart enable disable).each { |verb| 
+    desc "#{verb.capitalize} the Unicorn systemd service on the remote server"
+    
+    task verb.to_sym do
+    
+      if is_unicorn_user_installation?
+        
+        run :remote do 
+          command_systemd verb
+        end
+
+      else
+        
+        unicorn_elevate do 
+          run :remote do 
+            command_systemd verb
+          end
+        end
+      
+      end
+    
+    end
+    
+  }
+    
   # Returns the path to the template to use. Depending on whether the internal template was customized.
   def template_path
   
@@ -77,14 +108,41 @@ namespace :unicorn do
     
     File.exist?(custom_path) ? custom_path : original_path
   end
+
+  def is_unicorn_user_installation?
+    case fetch(:unicorn_system_or_user)
+    when "user" 
+      true
+    when "system"
+      false
+    else
+      raise "Undefined unicorn_system_or_user value. Must be 'user' or 'system'."
+    end
+  end
+
+  def command_systemd_raw verb
+    return %(systemctl#{" --user" if is_unicorn_user_installation?} #{verb} #{fetch :unicorn_service_name})
+  end
+
+  # Execute the systemd verb action and if it fails print the error information
+  def command_systemd verb
+    c = %((#{command_systemd_raw(verb)} && #{command_systemd_raw("status")}) || journalctl #{" --user" if is_unicorn_user_installation?} --no-pager _SYSTEMD_INVOCATION_ID=`systemctl #{" --user" if is_unicorn_user_installation?} show -p InvocationID --value #{fetch :unicorn_service_name}`)
+
+    comment c
+    command c
+  end
   
-  def elevate 
-  
+  def unicorn_elevate 
+
     user = fetch(:user)
     setup_user = fetch(:setup_user, user)
-    if setup_user != user
+    if setup_user && setup_user != user
       comment %{Switching to setup_user (#{setup_user})}
       set :user, setup_user
+      yield
+      set :user, user
+    else
+      yield
     end
   
   end
@@ -99,7 +157,8 @@ namespace :unicorn do
   desc "Print current Unicorn systemd service config from remote"
   task :print_remote do
     unicorn_systemd_config_path = fetch :unicorn_systemd_config_path
-    command %(cat #{unicorn_systemd_config_path})
+    comment %(Printing content of #{unicorn_systemd_config_path} from remote server)
+    command %(cat #{unicorn_systemd_config_path} || echo "Hello")
   end
 
 end
